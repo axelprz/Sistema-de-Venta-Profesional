8612e7860311cee095e2387297cc054a3f691db3
Axel Perez <152705784+axelprz@users.noreply.github.com>
2024-04-18 11:35:33 -0300

    Feature: Añadiendo la clase main para probar el login

---
+++ SistemaVentas/src/sistemaVentas/SistemaVentas.java
@@ -0,0 +1,11 @@
+
+package sistemaVentas;
+
+import Views.FrmLogin;
+
+public class SistemaVentas {
+    public static void main(String[] args) {
+        FrmLogin frm = new FrmLogin();
+        frm.setVisible(true);
+    }
+}
